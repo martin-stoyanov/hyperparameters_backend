ab5b0cc4ebc70e66103846f376afc53bf000921d
@martin.a.stoyanov <martin.a.stoyanov@gmail.com>
2019-09-01 16:01:09 -0400

    added whitenoise

--- hpjs_django/settings.py
+++ hpjs_django/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'hpjs_django.urls'
@@ -137,8 +138,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'hpjs_django/static')
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'hpjs_django/static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
